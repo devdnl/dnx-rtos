83f9b60ed9489c59dffb928d8b4c4fae8ac031a6
Daniel <danz@jabster.pl>
2012-10-30 18:09:49 +0100

    svgfile: branch start: Start working on "measd" daemon which will create graph svg file.

---
+++ app/measd/measd.h
@@ -0,0 +1,72 @@
+#ifndef CLEAR_H_
+#define CLEAR_H_
+/*=============================================================================================*//**
+@file    clear.h
+
+@author  Daniel Zorychta
+
+@brief
+
+@note    Copyright (C) 2012 Daniel Zorychta <daniel.zorychta@gmail.com>
+
+         This program is free software; you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the  Free Software  Foundation;  either version 2 of the License, or
+         any later version.
+
+         This  program  is  distributed  in the hope that  it will be useful,
+         but  WITHOUT  ANY  WARRANTY;  without  even  the implied warranty of
+         MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+         GNU General Public License for more details.
+
+         You  should  have received a copy  of the GNU General Public License
+         along  with  this  program;  if not,  write  to  the  Free  Software
+         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+*//*==============================================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================================================================================================
+                                            Include files
+==================================================================================================*/
+#include "system.h"
+
+
+/*==================================================================================================
+                                 Exported symbolic constants/macros
+==================================================================================================*/
+/** define stack size for this application */
+#define CLEAR_STACK_SIZE               (2 * MINIMAL_STACK_SIZE)
+
+/** define application name */
+#define CLEAR_NAME                     "clear"
+
+
+/*==================================================================================================
+                                  Exported types, enums definitions
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                     Exported object declarations
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                     Exported function prototypes
+==================================================================================================*/
+extern APPLICATION(clear);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CLEAR_H_ */
+/*==================================================================================================
+                                            End of file
+==================================================================================================*/
