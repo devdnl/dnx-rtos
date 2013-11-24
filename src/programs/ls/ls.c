445e138dcacb614e440339643ee38e5c3b0501a8
Daniel Zorychta <danz@jabster.pl>
2013-11-24 16:09:05 +0100

    feature/mkfifo-in-devfs: LS: Changed device sign from m to c.

--- src/programs/ls/ls.c
+++ src/programs/ls/ls.c
@@ -98,7 +98,7 @@ PROGRAM_MAIN(ls, int argc, char *argv[])
 
                         switch (dirent.filetype) {
                         case FILE_TYPE_DIR:     type = FONT_COLOR_YELLOW"d";  break;
-                        case FILE_TYPE_DRV:     type = FONT_COLOR_MAGENTA"m"; break;
+                        case FILE_TYPE_DRV:     type = FONT_COLOR_MAGENTA"c"; break;
                         case FILE_TYPE_LINK:    type = FONT_COLOR_CYAN"l";    break;
                         case FILE_TYPE_REGULAR: type = FONT_COLOR_GREEN" ";   break;
                         case FILE_TYPE_PROGRAM: type = FONT_BOLD"x";          break;
