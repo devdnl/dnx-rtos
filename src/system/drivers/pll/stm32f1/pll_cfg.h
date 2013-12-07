3adc5c18baaf191518b261833cd1f99d36295f3d
Daniel Zorychta <danz@jabster.pl>
2013-12-07 11:34:13 +0100

    feature/pll_get_freq: Corrected configuration according to RCC driver.

--- src/system/drivers/pll/stm32f1/pll_cfg.h
+++ src/system/drivers/pll/stm32f1/pll_cfg.h
@@ -36,6 +36,8 @@ extern "C" {
 ==============================================================================*/
 #include "config.h"
 
+//#undef STM32F10X_CL
+
 /*==============================================================================
   Exported symbolic constants/macros
 ==============================================================================*/
@@ -43,68 +45,80 @@ extern "C" {
  * OSCILLATORS
  *----------------------------------------------------------------------------*/
 /*
- * LSI enable (1) or disable (0)
+ * LSI
+ * ENABLE : enable oscillator
+ * DISABLE: disable oscillator
  */
-#define _PLL_CFG__LSI_ON                0
+#define _PLL_CFG__LSI_ON                DISABLE
 
 /*
- * LSE enable (1) or disable (0)
+ * LSE
+ * RCC_LSE_OFF   : LSE oscillator OFF
+ * RCC_LSE_ON    : LSE oscillator ON
+ * RCC_LSE_Bypass: LSE oscillator bypassed with external clock
  */
-#define _PLL_CFG__LSE_ON                0
+#define _PLL_CFG__LSE_ON                RCC_LSE_OFF
 
 /*
- * HSE enable (1) or disable (0)
+ * HSE
+ * RCC_HSE_OFF   : HSE oscillator OFF
+ * RCC_HSE_ON    : HSE oscillator ON
+ * RCC_HSE_Bypass: HSE oscillator bypassed with external clock
  */
-#define _PLL_CFG__HSE_ON                1
-
-/*
- * HSE bypass enable (1) or disable (0)
- *
- * Note: can be enabled only if HSE is disabled
- */
-#define _PLL_CFG__HSE_BYPASS_ON         0
-
+#define _PLL_CFG__HSE_ON                RCC_HSE_ON
 
 /*------------------------------------------------------------------------------
  * CLOCK SOURCES
  *----------------------------------------------------------------------------*/
 /*
  * RTC Clock source
- * 0: LSI
- * 1: LSE
- * 2: HSI/128
+ * RCC_RTCCLKSource_LSE       : LSE selected as RTC clock
+ * RCC_RTCCLKSource_LSI       : LSI selected as RTC clock
+ * RCC_RTCCLKSource_HSE_Div128: HSE clock divided by 128 selected as RTC clock
  */
-#define _PLL_CFG__RTCCLK_SRC            0
+#define _PLL_CFG__RTCCLK_SRC            RCC_RTCCLKSource_LSI
 
 /*
  * System clock source
- * 0: HSE
- * 1: HSI
- * 2: PLLCLK
+ * RCC_SYSCLKSource_HSI   : HSI selected as system clock
+ * RCC_SYSCLKSource_HSE   : HSE selected as system clock
+ * RCC_SYSCLKSource_PLLCLK: PLL selected as system clock
  */
-#define _PLL_CFG__SYSCLK_SRC            2
+#define _PLL_CFG__SYSCLK_SRC            RCC_SYSCLKSource_HSE
 
+#ifdef STM32F10X_CL
 /*
  * MCO source
- * 0: NO CLOCK
- * 1: HSE
- * 2: HSI
- * 3: SYSCLK
- * 4: PLLCLK/2
- * 5: PLL2CLK                           [Connectivity line only]
- * 6: PLL3CLK/2                         [Connectivity line only]
- * 7: PLL3CLK                           [Connectivity line only]
- * 8: XTAL                              [Connectivity line only]
+ * RCC_MCO_NoClock     : No clock selected
+ * RCC_MCO_SYSCLK      : System clock selected
+ * RCC_MCO_HSI         : HSI oscillator clock selected
+ * RCC_MCO_HSE         : HSE oscillator clock selected
+ * RCC_MCO_PLLCLK_Div2 : PLL clock divided by 2 selected
+ * RCC_MCO_PLL2CLK     : PLL2 clock selected
+ * RCC_MCO_PLL3CLK_Div2: PLL3 clock divided by 2 selected
+ * RCC_MCO_XT1         : External 3-25 MHz oscillator clock selected
+ * RCC_MCO_PLL3CLK     : PLL3 clock selected
  */
-#define _PLL_CFG__MCO_SRC               0
+#define _PLL_CFG__MCO_SRC               RCC_MCO_NoClock
+#else
+/*
+ * MCO source
+ * RCC_MCO_NoClock     : No clock selected
+ * RCC_MCO_SYSCLK      : System clock selected
+ * RCC_MCO_HSI         : HSI oscillator clock selected
+ * RCC_MCO_HSE         : HSE oscillator clock selected
+ * RCC_MCO_PLLCLK_Div2 : PLL clock divided by 2 selected
+ */
+#define _PLL_CFG__MCO_SRC               RCC_MCO_NoClock
+#endif
 
 #ifdef STM32F10X_CL
 /*
  * I2S clock source [Connectivity line]
- * 0: SYSCLK
- * 1: 2 x PLL3CLK
+ * RCC_I2S2CLKSource_SYSCLK  : system clock selected as I2S2 clock entry
+ * RCC_I2S2CLKSource_PLL3_VCO: PLL3 VCO clock selected as I2S2 clock entry
  */
-#define _PLL_CFG__I2S_SRC               0
+#define _PLL_CFG__I2S_SRC               RCC_I2S2CLKSource_SYSCLK
 #endif
 
 /*------------------------------------------------------------------------------
@@ -112,99 +126,117 @@ extern "C" {
  *----------------------------------------------------------------------------*/
 #ifdef STM32F10X_CL
 /*
- * PLL pre-divider 1 source (PREDIV1SRC) [Connectivity line]
- * 0: HSE
- * 1: PLL2CLK
+ * PLL pre-divider 1 source
+ * RCC_PREDIV1_Source_HSE : HSE selected as PREDIV1 clock
+ * RCC_PREDIV1_Source_PLL2: PLL2 selected as PREDIV1 clock
  */
-#define _PLL_CFG__PLL_PREDIV_SRC        0
+#define _PLL_CFG__PREDIV1_SRC           RCC_PREDIV1_Source_HSE
 #endif
 
 #ifdef STM32F10X_CL
 /*
- * PLL pre-divider 1 (PREDIV1) [Connectivity line]
- * 1-16: divider value
+ * PLL pre-divider 1 value
+ * RCC_PREDIV1_Div1-16: divider value
  */
-#define _PLL_CFG__PLL_PREDIV            1
+#define _PLL_CFG__PREDIV1_VAL           RCC_PREDIV1_Div1
 #endif
 
-#ifndef STM32F10X_CL
 /*
- * PLL pre-divider (PLLXTPRE) [Low-, medium-, high- and XL-density]
- * 0: HSE
- * 1: HSE/2
+ * PLL
+ * ENABLE : enable main PLL
+ * DISABLE: disable main PLL
  */
-#define _PLL_CFG__PLL_XTPRE_SRC         0
-#endif
+#define _PLL_CFG__PLL_ON                ENABLE
 
+#ifdef STM32F10X_CL
 /*
  * PLL clock source (PLLSRC)
- * 0: HSI/2
- * 1: PLLXTPRE                          [Low-, medium-, high- and XL-density]
- * 1: PREDIV1                           [Connectivity line]
+ * RCC_PLLSource_HSI_Div2: HSI oscillator clock divided by 2 selected as PLL clock entry
+ * RCC_PLLSource_PREDIV1 : PREDIV1 clock selected as PLL clock entry
  */
-#define _PLL_CFG__PLL_SRC               1
+#define _PLL_CFG__PLL_SRC               RCC_PLLSource_PREDIV1
+#else
+/*
+ * PLL clock source (PLLSRC)
+ * RCC_PLLSource_HSI_Div2: HSI oscillator clock divided by 2 selected as PLL clock entry
+ * RCC_PLLSource_HSE_Div1: HSE oscillator clock selected as PLL clock entry
+ * RCC_PLLSource_HSE_Div2: HSE oscillator clock divided by 2 selected as PLL clock entry
+ */
+#define _PLL_CFG__PLL_SRC               RCC_PLLSource_HSE_Div1
+#endif
 
+#ifdef STM32F10X_CL
+/*
+ * PLL multiplication factor (PLLMUL)
+ * RCC_PLLMul_x where x:{[4,9], 6_5}
+ */
+#define _PLL_CFG__PLL_MUL               RCC_PLLMul_9
+#else
 /*
  * PLL multiplication (PLLMUL)
- * 2-16                                 [Low-, medium-, high- and XL-density]
- * 4-9                                  [Connectivity line]
- * 10: x6.5                             [Connectivity line]
+ * RCC_PLLMul_x where x:[2,16]          [Low-, medium-, high- and XL-density]
  */
-#define _PLL_CFG__PLL_MUL               9
+#define _PLL_CFG__PLL_MUL               RCC_PLLMul_9
+#endif
 
+#ifdef STM32F10X_CL
 /*
  * USB prescaler
- * 1: PLLCLK / 1                        [Low-, medium-, high- and XL-density]
- * 2: PLLCLK / 1.5                      [Low-, medium-, high- and XL-density]
- * 2: (2 x PLLCLK) / 2                  [Connectivity line]
- * 3: (2 x PLLCLK) / 3                  [Connectivity line]
+ * RCC_OTGFSCLKSource_PLLVCO_Div3: PLL VCO clock divided by 2 selected as USB OTG FS clock source
+ * RCC_OTGFSCLKSource_PLLVCO_Div2: PLL VCO clock divided by 2 selected as USB OTG FS clock source
  */
-#define _PLL_CFG__USB_DIV               3
+#define _PLL_CFG__USB_DIV               RCC_OTGFSCLKSource_PLLVCO_Div3
+#else
+/*
+ * USB prescaler
+ * RCC_USBCLKSource_PLLCLK_1Div5 : PLL clock divided by 1,5 selected as USB clock source
+ * RCC_USBCLKSource_PLLCLK_Div1  : PLL clock selected as USB clock source
+ */
+#define _PLL_CFG__USB_DIV               RCC_USBCLKSource_PLLCLK_1Div5
+#endif
 
 
+#ifdef STM32F10X_CL
 /*------------------------------------------------------------------------------
  * PLL2 CONFIGURATION
  *----------------------------------------------------------------------------*/
-#ifdef STM32F10X_CL
 /*
- * PLL2 enable (1) or disable (0) [Connectivity line]
+ * PLL2/3 pre-divider (PREDIV2)
+ * RCC_PREDIV2_Divx where x:[1,16]
  */
-#define _PLL_CFG__PLL2_ON               0
+#define _PLL_CFG__PLL_PREDIV2           RCC_PREDIV2_Div1
 
 /*
- * PLL2 multiplication [Connectivity line]
- * 8-14: multiplier range
- * 16  : multiplier value
- * 20  : multiplier value
+ * PLL2
+ * ENABLE : enable PLL2
+ * DISABLE: disable PLL2
  */
-#define _PLL_CFG__PLL2_MUL              8
+#define _PLL_CFG__PLL2_ON               DISABLE
 
 /*
- * PLL2/3 pre-divider (PREDIV2) [Connectivity line]
- * 1-16: divider range
+ * PLL2 multiplication factor
+ * RCC_PLL2Mul_x where x:{[8,14], 16, 20}
  */
-#define _PLL_CFG__PLL23_PREDIV          1
-
+#define _PLL_CFG__PLL2_MUL              RCC_PLL2Mul_8
 #endif
 
 
+#ifdef STM32F10X_CL
 /*------------------------------------------------------------------------------
  * PLL3 CONFIGURATION
  *----------------------------------------------------------------------------*/
-#ifdef STM32F10X_CL
 /*
- * PLL3 enable (1) or disable (0) [Connectivity line]
+ * PLL3
+ * ENABLE : enable PLL3
+ * DISABLE: disable PLL3
  */
-#define _PLL_CFG__PLL3_ON               0
+#define _PLL_CFG__PLL3_ON               DISABLE
 
 /*
- * PLL3 multiplication [Connectivity line]
- * 8-14: multiplier range
- * 16  : multiplier value
- * 20  : multiplier value
+ * PLL3 multiplication factor
+ * RCC_PLL3Mul_x where x:{[8,14], 16, 20}
  */
-#define _PLL_CFG__PLL3_MUL              8
-
+#define _PLL_CFG__PLL3_MUL              RCC_PLL3Mul_8
 #endif
 
 
@@ -212,28 +244,47 @@ extern "C" {
  * SYSTEM PRESCALERS
  *----------------------------------------------------------------------------*/
 /*
- * AHB prescaler (in: SYSCLK)
- * 1-512: prescaler range
+ * AHB prescaler (in: SYSCLK; out: HCLK)
+ * RCC_SYSCLK_Div1  : AHB clock = SYSCLK
+ * RCC_SYSCLK_Div2  : AHB clock = SYSCLK/2
+ * RCC_SYSCLK_Div4  : AHB clock = SYSCLK/4
+ * RCC_SYSCLK_Div8  : AHB clock = SYSCLK/8
+ * RCC_SYSCLK_Div16 : AHB clock = SYSCLK/16
+ * RCC_SYSCLK_Div64 : AHB clock = SYSCLK/64
+ * RCC_SYSCLK_Div128: AHB clock = SYSCLK/128
+ * RCC_SYSCLK_Div256: AHB clock = SYSCLK/256
+ * RCC_SYSCLK_Div512: AHB clock = SYSCLK/512
  */
-#define _PLL_CFG__AHB_PRE               1
+#define _PLL_CFG__AHB_PRE               RCC_SYSCLK_Div1
 
 /*
- * APB1 prescaler (in: clock from AHB prescaler)
- * 1, 2, 4, 8, 16: prescaler values
+ * APB1 prescaler (in: HCLK; out: PCLK1) (low speed)
+ * RCC_HCLK_Div1 : APB1 clock = HCLK
+ * RCC_HCLK_Div2 : APB1 clock = HCLK/2
+ * RCC_HCLK_Div4 : APB1 clock = HCLK/4
+ * RCC_HCLK_Div8 : APB1 clock = HCLK/8
+ * RCC_HCLK_Div16: APB1 clock = HCLK/16
  */
-#define _PLL_CFG__APB1_PRE              2
+#define _PLL_CFG__APB1_PRE              RCC_HCLK_Div2
 
 /*
- * APB2 prescaler (in: clock from AHB prescaler)
- * 1, 2, 4, 8, 16: prescaler values
+ * APB2 prescaler (in: HCLK; out: PCLK2) (high speed)
+ * RCC_HCLK_Div1 : APB1 clock = HCLK
+ * RCC_HCLK_Div2 : APB1 clock = HCLK/2
+ * RCC_HCLK_Div4 : APB1 clock = HCLK/4
+ * RCC_HCLK_Div8 : APB1 clock = HCLK/8
+ * RCC_HCLK_Div16: APB1 clock = HCLK/16
  */
-#define _PLL_CFG__APB2_PRE              1
+#define _PLL_CFG__APB2_PRE              RCC_HCLK_Div1
 
 /*
- * ADC prescaler (in: clock from APB2 prescaler)
- * 2, 4, 8: prescaler values
+ * ADC prescaler (in: PCLK2; out: ADCCLK)
+ * RCC_PCLK2_Div2: ADC clock = PCLK2/2
+ * RCC_PCLK2_Div4: ADC clock = PCLK2/4
+ * RCC_PCLK2_Div6: ADC clock = PCLK2/6
+ * RCC_PCLK2_Div8: ADC clock = PCLK2/8
  */
-#define _PLL_CFG__ADC_PRE               8
+#define _PLL_CFG__ADC_PRE               RCC_PCLK2_Div8
 
 /*
  * Flash Latency (Flash wait-state)
