39e70cf20b84834774a41b8fde36c14564f18200
Daniel Zorychta <danz@jabster.pl>
2014-04-09 20:37:12 +0200

    feature/config_tool: Added licence header.

--- config/stm32f1/gpio_flags.h
+++ config/stm32f1/gpio_flags.h
@@ -1,3 +1,28 @@
+/*=========================================================================*//**
+@file    gpio_flags.h
+
+@author  Daniel Zorychta
+
+@brief   GPIO module configuration flags.
+
+@note    Copyright (C) 2014 Daniel Zorychta <daniel.zorychta@gmail.com>
+
+         This program is free software; you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the  Free Software  Foundation;  either version 2 of the License, or
+         any later version.
+
+         This  program  is  distributed  in the hope that  it will be useful,
+         but  WITHOUT  ANY  WARRANTY;  without  even  the implied warranty of
+         MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+         GNU General Public License for more details.
+
+         You  should  have received a copy  of the GNU General Public License
+         along  with  this  program;  if not,  write  to  the  Free  Software
+         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+*//*==========================================================================*/
 #ifndef _GPIO_FLAGS_H_
 #define _GPIO_FLAGS_H_
 
@@ -347,3 +372,6 @@
 #define __GPIO_PG_PIN_15_STATE__ _FLOAT
 
 #endif /* _GPIO_FLAGS_H_ */
+/*==============================================================================
+  End of file
+==============================================================================*/
