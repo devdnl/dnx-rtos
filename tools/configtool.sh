ed63eff540275abdc0146982589f54f0bd5881ff
Daniel Zorychta <danz@jabster.pl>
2014-07-24 23:35:19 +0200

    feature/new_config_tool: Corrected script starting configtool. Repleaced UPX libraries by not UPXed in the i686.

--- tools/configtool.sh
+++ tools/configtool.sh
@@ -8,11 +8,9 @@ script="./configtool.lua"
 
 if [ "$os" = "Linux" ]; then
         if [ "$arch" = "x86_64" ] || [ "$arch" = "amd64" ]; then
-                LD_LIBRARY_PATH="./wxLua_linux_x86_64":$LD_LIBRARY_PATH
-                ./wxLua_linux_x86_64/wxLua $script
+                LD_LIBRARY_PATH="./wxLua_linux_x86_64":$LD_LIBRARY_PATH ./wxLua_linux_x86_64/wxLua $script
         else
-                LD_LIBRARY_PATH="./wxLua_linux_i386":$LD_LIBRARY_PATH
-                ./wxLua_linux_i386/wxLua $script
+                LD_LIBRARY_PATH="./wxLua_linux_i686":$LD_LIBRARY_PATH ./wxLua_linux_i386/wxLua $script
         fi
 elif [[ "$os" =~ '_NT' ]]; then
         ./wxLua_win32/wxLua $script
