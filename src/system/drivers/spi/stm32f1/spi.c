0f9c6b3728a2d481e96d3974bddcc9ac6dd34a91
Daniel Zorychta <danz@jabster.pl>
2014-10-08 15:56:10 +0200

    develop: Added = sign after [] declaration in tables. Example: [EINVAL] = 5, .

--- src/system/drivers/spi/stm32f1/spi.c
+++ src/system/drivers/spi/stm32f1/spi.c
@@ -789,21 +789,21 @@ static void turn_off_SPI(u8_t major)
 static void apply_SPI_config(struct spi_virtual *vspi)
 {
         const u16_t divider_mask[SPI_CLK_DIV_256 + 1] = {
-                [SPI_CLK_DIV_2  ] 0x00,
-                [SPI_CLK_DIV_4  ] SPI_CR1_BR_0,
-                [SPI_CLK_DIV_8  ] SPI_CR1_BR_1,
-                [SPI_CLK_DIV_16 ] SPI_CR1_BR_1 | SPI_CR1_BR_0,
-                [SPI_CLK_DIV_32 ] SPI_CR1_BR_2,
-                [SPI_CLK_DIV_64 ] SPI_CR1_BR_2 | SPI_CR1_BR_0,
-                [SPI_CLK_DIV_128] SPI_CR1_BR_2 | SPI_CR1_BR_1,
-                [SPI_CLK_DIV_256] SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0,
+                [SPI_CLK_DIV_2  ] = 0x00,
+                [SPI_CLK_DIV_4  ] = SPI_CR1_BR_0,
+                [SPI_CLK_DIV_8  ] = SPI_CR1_BR_1,
+                [SPI_CLK_DIV_16 ] = SPI_CR1_BR_1 | SPI_CR1_BR_0,
+                [SPI_CLK_DIV_32 ] = SPI_CR1_BR_2,
+                [SPI_CLK_DIV_64 ] = SPI_CR1_BR_2 | SPI_CR1_BR_0,
+                [SPI_CLK_DIV_128] = SPI_CR1_BR_2 | SPI_CR1_BR_1,
+                [SPI_CLK_DIV_256] = SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0,
         };
 
         const u16_t spi_mode_mask[SPI_MODE_3 + 1] = {
-                [SPI_MODE_0] 0x00,
-                [SPI_MODE_1] SPI_CR1_CPHA,
-                [SPI_MODE_2] SPI_CR1_CPOL,
-                [SPI_MODE_3] SPI_CR1_CPOL | SPI_CR1_CPHA
+                [SPI_MODE_0] = 0x00,
+                [SPI_MODE_1] = SPI_CR1_CPHA,
+                [SPI_MODE_2] = SPI_CR1_CPOL,
+                [SPI_MODE_3] = SPI_CR1_CPOL | SPI_CR1_CPHA
         };
 
         SPI_t *SPI = SPI_cfg[vspi->major].SPI;
