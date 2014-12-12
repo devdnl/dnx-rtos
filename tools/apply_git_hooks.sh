c015ec6fbb9ed75a68ffc98acb9c0952dc118485
Daniel Zorychta <danz@jabster.pl>
2014-12-12 20:35:51 +0100

    release/Release-1.5.0: Added checking that project is in the git repositiory before copy git hooks.

--- tools/apply_git_hooks.sh
+++ tools/apply_git_hooks.sh
@@ -4,4 +4,6 @@ cd $(dirname $0)
 
 git_hook_path=../extra/git_hooks/commit-msg
 
-/bin/cp -u $git_hook_path ../.git/hooks
+if [ -d "../.git/hooks" ]; then
+   /bin/cp -u $git_hook_path ../.git/hooks
+fi
