0151052de7b44f6bb51b3f33c586c369ae8eb8d6
Daniel Zorychta <danzor87@gmail.com>
2017-05-29 20:31:15 +0200

    develop: Added DCI files :P

---
+++ src/system/drivers/dci/dci_ioctl.h
@@ -0,0 +1,127 @@
+/*==============================================================================
+File    dci_ioctl.h
+
+Author  Daniel Zorychta
+
+Brief   Digital Camera Interface Driver
+
+        Copyright (C) 2017 Daniel Zorychta <daniel.zorychta>
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation and modified by the dnx RTOS exception.
+
+        NOTE: The modification  to the GPL is  included to allow you to
+              distribute a combined work that includes dnx RTOS without
+              being obliged to provide the source  code for proprietary
+              components outside of the dnx RTOS.
+
+        The dnx RTOS  is  distributed  in the hope  that  it will be useful,
+        but WITHOUT  ANY  WARRANTY;  without  even  the implied  warranty of
+        MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+        GNU General Public License for more details.
+
+        Full license text is available on the following file: doc/license.txt.
+
+==============================================================================*/
+
+/**
+@defgroup drv-dci DCI Driver
+
+\section drv-dci-desc Description
+Driver handles Digital Camera Interface.
+
+\section drv-dci-sup-arch Supported architectures
+\li stm32f4
+
+\section drv-dci-ddesc Details
+\subsection drv-dci-ddesc-num Meaning of major and minor numbers
+Major number means peripheral number of camera interface (camera0, camera1, etc).
+Minor number should be set to 0.
+
+\subsubsection drv-dci-ddesc-numres Numeration restrictions
+The STM32F4 support only one camera interface, so major number is limited to 0.
+Minor number has no meaning and should be set always to 0.
+
+\subsection drv-dci-ddesc-init Driver initialization
+To initialize driver the following code can be used:
+
+@code
+driver_init("DCI", 0, 0, "/dev/video");
+@endcode
+
+\subsection drv-dci-ddesc-release Driver release
+To release driver the following code can be used:
+@code
+driver_release("DCI", 0, 0);
+@endcode
+
+\subsection drv-dci-ddesc-cfg Driver configuration
+Only static configuration is possible via configuration files (Configtool).
+
+\subsection drv-dci-ddesc-write Data write
+Writing is not possible.
+
+\subsection drv-dci-ddesc-read Data read
+Only full frame can be read at once. Other transfer sizes are not supported.
+
+\subsection drv-dci-ddesc-ioctl Driver parameters
+User can get current driver parameters (related with frame size) by using
+ioctl() function with IOCTL_DCI__GET_PARAMS request.
+
+@{
+*/
+
+#ifndef _DCI_IOCTL_H_
+#define _DCI_IOCTL_H_
+
+/*==============================================================================
+  Include files
+==============================================================================*/
+#include "drivers/ioctl_macros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==============================================================================
+  Exported macros
+==============================================================================*/
+/**
+ * @brief  Get image parameters.
+ * @param  [RD] @ref DCI_params_t*        image parameters
+ * @return On success 0 is returned, otherwise -1 and @ref errno code is set.
+ */
+#define IOCTL_DCI__GET_PARAMS           _IOR(DCI, 0x00, DCI_params_t*)
+
+/*==============================================================================
+  Exported object types
+==============================================================================*/
+typedef struct {
+        u16_t x_resolution;             /*!< X camera resolution */
+        u16_t y_resolution;             /*!< Y camera resolution */
+        u8_t  bytes_per_pixel;          /*!< Bytes per pixel     */
+        bool  JPEG_mode;                /*!< JPEG mode           */
+} DCI_params_t;
+
+/*==============================================================================
+  Exported objects
+==============================================================================*/
+
+/*==============================================================================
+  Exported functions
+==============================================================================*/
+
+/*==============================================================================
+  Exported inline functions
+==============================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _DCI_IOCTL_H_ */
+/**@}*/
+/*==============================================================================
+  End of file
+==============================================================================*/
---
+++ src/system/drivers/dci/stm32f4/dci_cfg.h
@@ -0,0 +1,89 @@
+/*==============================================================================
+File     dcmi_cfg.h
+
+Author   Daniel Zorychta
+
+Brief    Digital Camera Interface Driver
+
+         Copyright (C) 2017 Daniel Zorychta <daniel.zorychta>
+
+         This program is free software; you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the  Free Software  Foundation;  either version 2 of the License, or
+         any later version.
+
+         This  program  is  distributed  in the hope that  it will be useful,
+         but  WITHOUT  ANY  WARRANTY;  without  even  the implied warranty of
+         MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+         GNU General Public License for more details.
+
+         You  should  have received a copy  of the GNU General Public License
+         along  with  this  program;  if not,  write  to  the  Free  Software
+         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+==============================================================================*/
+
+#ifndef _DCI_CFG_H_
+#define _DCI_CFG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==============================================================================
+  Include files
+==============================================================================*/
+#include "config.h"
+
+/*==============================================================================
+  Exported macros
+==============================================================================*/
+#define _DCI_CAM_RES_X         __DCI_CAM_RES_X__
+#define _DCI_CAM_RES_Y         __DCI_CAM_RES_Y__
+#define _DCI_BYTES_PER_PIXEL   __DCI_BYTES_PER_PIXEL__
+#define _DCI_EDM               __DCI_EDM__
+#define _DCI_FCRC              __DCI_FCRC__
+#define _DCI_VSPOL             __DCI_VSPOL__
+#define _DCI_HSPOL             __DCI_HSPOL__
+#define _DCI_PCKPOL            __DCI_PCKPOL__
+#define _DCI_JPEG              __DCI_JPEG__
+#define _DCI_ESS               __DCI_ESS__
+#define _DCI_FEC               __DCI_FEC__
+#define _DCI_FEU               __DCI_FEU__
+#define _DCI_LEC               __DCI_LEC__
+#define _DCI_LEU               __DCI_LEU__
+#define _DCI_LSC               __DCI_LSC__
+#define _DCI_LSU               __DCI_LSU__
+#define _DCI_FSC               __DCI_FSC__
+#define _DCI_FSU               __DCI_FSU__
+#define _DCI_CROP              __DCI_CROP__
+#define _DCI_CROP_START_X      __DCI_CROP_START_X__
+#define _DCI_CROP_START_Y      __DCI_CROP_START_Y__
+#define _DCI_CROP_HEIGHT       __DCI_CROP_HEIGHT__
+#define _DCI_CROP_WIDTH        __DCI_CROP_WIDTH__
+
+/*==============================================================================
+  Exported object types
+==============================================================================*/
+
+/*==============================================================================
+  Exported objects
+==============================================================================*/
+
+/*==============================================================================
+  Exported functions
+==============================================================================*/
+
+/*==============================================================================
+  Exported inline functions
+==============================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _DCI_CFG_H_ */
+/*==============================================================================
+  End of file
+==============================================================================*/
