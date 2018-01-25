3edbebb856f9f4c5aa559d1d00556c8fa3ea1c9d
Daniel Zorychta <danzor87@gmail.com>
2018-01-25 18:15:38 +0100

    feature/romfs: Generate content of file.

--- src/system/fs/romfs/romfsmap.py
+++ src/system/fs/romfs/romfsmap.py
@@ -1,34 +1,76 @@
 import os
 import sys
 import binascii
+import array
+import re
 
 walk_dir = sys.argv[1]
+dest_dir = sys.argv[2]
 
-print('walk_dir = ' + walk_dir)
+def underscore(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+def file_arrayname(filepath):
+    return 'file_' + re.sub('[,.:/@+-\\\\*\(\)\[\]!#=]', '', underscore(filepath))
+
+def file2carray(filepath, outfile):
+
+    filebytes = open(filepath, "rb").read()
+
+    fout = open(outfile + '.c', "wb")
+    fout.write("// file generated\n")
+    fout.write("#include <stdint.h>\n\n")
+    fout.write("const uint8_t " + file_arrayname(filepath) + "["+ str(len(filebytes)) +"] = {\n    ")
+
+    ctr = 0
+    for byte in filebytes:
+        fout.write('0x' + binascii.hexlify(byte) + ', ')
+        ctr = ctr + 1
+        if ctr >= 16:
+            fout.write('\n    ')
+            ctr = 0
+
+    fout.write("\n};\n")
+    fout.close()
+
+def dir2carray(dirname, subdirs, files, outfile):
+    print("=>", dirname, subdirs, files, outfile)
+
+    outfile = os.path.join(outfile, dirname)
+
+    if outfile == dest_dir + '/':
+        outfile = os.path.join(outfile, "root")
+
+    fout = open(outfile + '.c', "wb")
+    fout.write("// file generated\n")
+    fout.write("#include <stdint.h>\n")
+    fout.write('#include "romfs_types.h"\n\n')
+    fout.close()
+
+
+# print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
 for root, subdirs, files in os.walk(walk_dir):
     print('--\nroot = ' + root)
-    list_file_path = os.path.join(root, 'x')
-    print('list_file_path = ' + list_file_path)
 
-    with open(list_file_path, 'wb') as list_file:
-        for subdir in subdirs:
+    dest_path_root = os.path.join(dest_dir, os.path.basename(root))
+
+    try:
+        os.mkdir(dest_path_root)
+    except:
+        pass
+
+    dir2carray(os.path.basename(root), subdirs, files, dest_path_root)
+
+    for subdir in subdirs:
             print('\t- subdirectory ' + subdir)
 
-        for filename in files:
+    for filename in files:
             file_path = os.path.join(root, filename)
+            dest_path = os.path.join(dest_path_root, filename)
 
             print('\t- file %s (full path: %s)' % (filename, file_path))
 
-            #with open(file_path, 'rb') as f:
-            #    f_content = f.read()
-            #    list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-            #    list_file.write(f_content)
-            #    list_file.write(b'\n')
+            file2carray(file_path, dest_path)
 
