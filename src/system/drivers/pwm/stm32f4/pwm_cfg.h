f9b13acc43885ea5aad1c23fe9959d2be3b6385c
Daniel Zorychta <danzor87@gmail.com>
2018-11-04 20:39:39 +0100

    develop: Added PWM driver for STM32F4 port.

---
+++ src/system/drivers/pwm/stm32f4/pwm_cfg.h
@@ -0,0 +1,68 @@
+/*==============================================================================
+File    pwm_cfg.h
+
+Author  Daniel Zorychta
+
+Brief   PWM driver
+
+        Copyright (C) 2018 Daniel Zorychta <daniel.zorychta@gmail.com>
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation and modified by the dnx RTOS exception.
+
+        NOTE: The modification  to the GPL is  included to allow you to
+              distribute a combined work that includes dnx RTOS without
+              being obliged to provide the source  code for proprietary
+              components outside of the dnx RTOS.
+
+        The dnx RTOS  is  distributed  in the hope  that  it will be useful,
+        but WITHOUT  ANY  WARRANTY;  without  even  the implied  warranty of
+        MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+        GNU General Public License for more details.
+
+        Full license text is available on the following file: doc/license.txt.
+
+==============================================================================*/
+
+#ifndef _PWM_CFG_H_
+#define _PWM_CFG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==============================================================================
+  Include files
+==============================================================================*/
+#include "config.h"
+
+/*==============================================================================
+  Exported macros
+==============================================================================*/
+#define _PWM_CFG__EXAMPLE          __PWM_CFG_EXAMPLE__
+
+/*==============================================================================
+  Exported object types
+==============================================================================*/
+
+/*==============================================================================
+  Exported objects
+==============================================================================*/
+
+/*==============================================================================
+  Exported functions
+==============================================================================*/
+
+/*==============================================================================
+  Exported inline functions
+==============================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PWM_CFG_H_ */
+/*==============================================================================
+  End of file
+==============================================================================*/
